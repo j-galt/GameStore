ff78254e4a40b40e78212f41baf3ce9a3337fcea
Illia Pivtoraiko <Illia_Pivtoraiko@epam.com>
2018-06-27 22:20:16 +0300

    add logging

---
+++ GameStore.Web/Infrastructure/CustomValidationFilter.cs
@@ -0,0 +1,63 @@
+﻿using GameStore.Web.Utils;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace GameStore.Web.Infrastructure
+{
+    public class CustomValidationFilter : IActionFilter
+    {
+        private readonly ILogger _log;
+
+        public CustomValidationFilter(ILogger log)
+        {
+            _log = log;
+        }
+
+        public bool AllowMultiple => true;
+
+        public Task<HttpResponseMessage> ExecuteActionFilterAsync(HttpActionContext context, CancellationToken cancellationToken, Func<Task<HttpResponseMessage>> continuation)
+        {
+            if (!context.ModelState.IsValid)
+            {
+                return Task.FromResult(WrapAndLogValidationError(context));
+            }
+            return continuation();
+        }
+
+        private HttpResponseMessage WrapAndLogValidationError(HttpActionContext actionContext)
+        {
+            var invalidProperties = actionContext.ModelState.Where(m => m.Value.Errors.Any());
+
+            var validationErrorCollection = invalidProperties.Select(x =>
+            {
+                var errors = x.Value.Errors.Select((e => String.IsNullOrEmpty(e.ErrorMessage)
+                        ? Regex.Replace(e.Exception.Message, @"\.(.*)", String.Empty)
+                        : e.ErrorMessage));
+
+                var prefix = String.IsNullOrEmpty(x.Key) ? "Summary" : x.Key;
+                return new { key = prefix, value = errors };
+
+            }).ToDictionary(x => x.key, x => x.value);
+
+            _log.Warn(LogMessageComposer.Compose(
+                new
+                {
+                    details = "Model validation failed",
+                    user = "Anonymous",
+                    modelState = validationErrorCollection
+                }));
+
+            throw new Exception("Resource validation failed.");
+        }
+    }
+}
